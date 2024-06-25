e6e9bf3da7206bee4f29db9ac704a9b0d234e265
dozagirugi <dozagirugi@gmail.com>
2024-06-25 13:44:21 +0900

    평점 순대로 출력하기

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -1,30 +1,30 @@
 import "./MovieCard.css";
 import Star from "../../assets/star.png";
 
-export default function MovieCard() {
+export default function MovieCard({ movie }) {
   return (
-    <a className="movie_card">
+    <a
+      href={`https://www.themoviedb.org/movie/${movie.id}?language=ko`}
+      target="_blank"
+      className="movie_card"
+    >
       <img
-        src="https://search1.kakaocdn.net/thumb/C232x324.fwebp.q100/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fmovie%2F5d47796c4b1c298723b25786ef9dc4f62fa0fed9"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt="Movie Poster"
         className="movie_poster"
       />
 
       <div className="movie_details">
-        <h3 className="movie_details_heading">콰이어트 플레이스</h3>
+        <h3 className="movie_details_heading">{movie.title}</h3>
         <div className="align_center movie_date_rate">
-          <p>2024.06.26</p>
+          <p>{movie.release_date}</p>
           <p className="align_center">
-            평점
+            {movie.vote_average}
             <img src={Star} alt="star emoji" className="card_emoji"></img>
           </p>
         </div>
         <p className="movie_description">
-          쉿, 살고 싶다면 절대 소리내지 말 것! 평균 소음 90 데시벨을 자랑하는
-          미국 최대도시 뉴욕, 고양이 ‘프로도’와 함께 간만에 외출을 나온
-          ‘사미라’는 공연을 보고 돌아가던 중 하늘에서 떨어져 내리는 섬광을
-          목격하고 곧 사람들을 닥치는 대로 공격하는 정체불명 괴생명체의 출현에
-          충격에 휩싸인다.
+          {movie.overview.slice(0, 100) + "..."}
         </p>
       </div>
     </a>
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,36 +2,102 @@ import "./MovieList.css";
 import Fire from "../../assets/fire.png";
 import MovieCard from "./MovieCard";
 
+import { useEffect, useState } from "react";
+
 export default function MovieList() {
+  const [movies, setMovies] = useState([]);
+  const [filterMovies, setFilterMovies] = useState([]);
+  const [minRating, setMinRating] = useState(0);
+  const [sort, setSort] = useState({
+    by: "default",
+    order: "asc",
+  });
+
+  const fetchMovies = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=90f95aa999b5d07928b9e6edf0d91e41&language=ko"
+    );
+    const data = await response.json();
+    setMovies(data.results);
+    setFilterMovies(data.results);
+    console.log(data.results);
+  };
+
+  const handleFilter = (rate) => {
+    setMinRating(rate);
+
+    const filtered = movies.filter((movie) => movie.vote_average >= rate);
+    setFilterMovies(filtered);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const handleSort = (e) => {
+    const { name, value } = e.target;
+    setSort((prev) => ({ ...prev, [name]: value }));
+  };
+  console.log(sort);
+
   return (
     <section className="movie_list">
       <header className="align_center movie_list_header">
         <h2 className="align_center movie_list_heading">
-          인기순 <img src={Fire} alt="Fire emoji" className="navbar_emoji" />
+          인기작품 <img src={Fire} alt="Fire emoji" className="navbar_emoji" />
         </h2>
 
         <div className="align_center movie_list_fs">
           <ul className="align_center movie_filter">
-            <li className="novie_filter_iterm active"> 8+ Star </li>
-            <li className="novie_filter_iterm"> 7+ Star </li>
-            <li className="novie_filter_iterm"> 6+ Star </li>
+            <li
+              className={
+                minRating === 8
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
+              onClick={() => handleFilter(8)}
+            >
+              8+ Star
+            </li>
+            <li
+              className={
+                minRating === 7
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
+              onClick={() => handleFilter(7)}
+            >
+              7+ Star
+            </li>
+            <li
+              className={
+                minRating === 6
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
+              onClick={() => handleFilter(6)}
+            >
+              6+ Star
+            </li>
           </ul>
 
-          <select name="" id="" className="movie_sorting">
-            <option value="">정렬 기준</option>
-            <option value="">개봉일 순</option>
-            <option value="">평점 순</option>
+          <select name="by" id="by" className="movie_sorting">
+            <option value="default">정렬 기준</option>
+            <option value="release_date">개봉일 순</option>
+            <option value="vote_average">평점 순</option>
           </select>
 
-          <select name="" id="" className="movie_sorting">
-            <option value="">오름차순</option>
-            <option value="">내림차순</option>
+          <select name="order" id="order" className="movie_sorting">
+            <option value="asc">오름차순</option>
+            <option value="desc">내림차순</option>
           </select>
         </div>
       </header>
 
       <div className="movie_cards">
-        <MovieCard />
+        {filterMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
     </section>
   );
